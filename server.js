7eac500562c3698bf01b180f9b1d835c4fb5adc0
Shekhar Singh <smehla147@gmail.com>
2024-07-19 10:44:39 +1000

    added the login and register page with bootstrap style

--- server.js
+++ server.js
@@ -2,10 +2,34 @@ import express from "express";
 import path from "path";
 const server = express();
 const __dirName = path.resolve();
-
+server.use(express.urlencoded({ extended: true }));
+// home page controller
 server.use(express.static(path.join(__dirName, "public")));
 server.get("/", (req, res) => {
+  console.log(req.query);
   res.sendFile(__dirName + "/index.html");
+});
+// login page controller
+
+server.get("/login", (req, res) => {
+  console.log(req.body);
+  res.sendFile(__dirName + "/login.html");
+});
+
+// handlig the data recived from the form
+server.post("/login", (req, res) => {
+  console.log(req.body);
+  res.sendFile(__dirName + "/login.html");
+});
+
+// // register page controller
+server.post("/register", (req, res) => {
+  console.log(req.query);
+  res.sendFile(__dirName + "/register.html");
+});
+server.get("/register", (req, res) => {
+  console.log(req.query);
+  res.sendFile(__dirName + "/register.html");
 });
 const PORT = 8000;
 server.listen(PORT, (err) => {
